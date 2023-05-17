e827c67d36ff7e4e1379184ddb24f622922983c8
Alexandra <alexandra.passos@asaas.com.br>
2023-05-17 14:28:53 -0300

    Adicionando classe customer dentro do domain

---
+++ grails-app/domain/com/coinCollector/Customer.groovy
@@ -0,0 +1,14 @@
+package com.coinCollector
+
+class Customer {
+    String name
+    String email
+    String cpfCnpj
+    String cep
+    String state 
+    String city
+    String district
+    String street
+    String complement
+    int houseNumber
+}
