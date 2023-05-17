853120105ef72b3780fc23bdddfdd7e307b33ed0
Alexandra <alexandra.passos@asaas.com.br>
2023-05-17 14:46:48 -0300

    Adicionando classe payer dentro do domain

---
+++ grails-app/domain/com/coinCollector/Payer.groovy
@@ -0,0 +1,14 @@
+package com.coinCollector
+
+class Payer {
+    String email
+    String cpfCnpj
+    String cep
+    String state
+    String city
+    String district
+    String street
+    String houseNumber
+    String complement
+    String phoneNumber
+}
