0676e6cf4735a7b28049d14f70c8b53511be5714
Eric <eric.garcia@asaas.com.br>
2023-05-19 19:57:18 -0300

    Criação de BaseEntity e associação com BasePerson

---
+++ src/main/groovy/utils/basePerson/BasePerson.groovy
@@ -0,0 +1,25 @@
+package utils.basePerson
+
+import utils.baseEntity.BaseEntity
+
+abstract class BasePerson extends BaseEntity{
+    String name
+
+    String email
+
+    String cpfCnpj
+
+    String cep
+
+    String state
+
+    String city 
+
+    String district
+
+    String address
+
+    String addressNumber
+    
+    String complement
+}
