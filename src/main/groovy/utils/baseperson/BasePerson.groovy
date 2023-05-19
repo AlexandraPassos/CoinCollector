3381a541996cf2a91fd0f6aed1a99bda04922ca2
Eric <eric.garcia@asaas.com.br>
2023-05-19 19:00:26 -0300

    Adicionando espaçamento entre atributos de BasePerson

--- src/main/groovy/utils/baseperson/BasePerson.groovy
+++ src/main/groovy/utils/baseperson/BasePerson.groovy
@@ -4,12 +4,20 @@ abstract class BasePerson {
     String name
 
     String email
+
     String cpfCnpj
+
     String cep
+
     String state
-    String city
+
+    String city 
+
     String district
+
     String street
+
     String houseNumber
+    
     String complement
 }
