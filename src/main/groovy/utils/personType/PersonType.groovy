15aa2f3ce53e5b205ae71abc2f2f162c13bf7b9c
Alexandra <alexandra.passos@asaas.com.br>
2023-05-24 15:45:13 -0300

    Alterando arquivo PersonType.groovy

--- src/main/groovy/utils/personType/PersonType.groovy
+++ src/main/groovy/utils/personType/PersonType.groovy
@@ -1,9 +1,9 @@
 package utils.personType
 
 enum PersonType { 
-    PJ('Pessoa Física'),
+    PJ('Pessoa Jurídica'),
     
-    PF('Pessoa Jurídica'),
+    PF('Pessoa Física'),
 
     final String id
     PersonType(String id) { this.id = id }
