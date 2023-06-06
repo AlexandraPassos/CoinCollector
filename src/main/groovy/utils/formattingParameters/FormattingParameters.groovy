c708a9296f29f42e9a9c8b5687731f8ede8fe9a8
Eric Garcia <eric.garcia@asaas.com.br>
2023-06-06 19:37:33 -0300

    Adicionando tratamento de nullPointer no método removeSpecialCharacters

--- src/main/groovy/utils/formattingParameters/FormattingParameters.groovy
+++ src/main/groovy/utils/formattingParameters/FormattingParameters.groovy
@@ -3,29 +3,20 @@ package utils.formattingParameters
 class FormattingParameters {
 
     public static String removeSpecialCharacters(String parameter) {
-        parameter = parameter.replaceAll("[^0-9]", "")
-        return parameter
+        if(parameter.isEmpty()) {
+            return parameter
+        } else {
+            parameter = parameter.replaceAll("[^0-9]", "")
+            return parameter
+        }
     }
 
     public static Boolean expectedLength(String parameter, Integer parameterLength) {
-        if (!(parameter.length() == parameterLength)) {
-            return false
-        } else {
-            return true
-        }
+        return parameter.length() == parameterLength
     }
 
     public static Boolean repeatSameCharacter(String parameter, int length) {
         String regexRepetedSequenceOnParameter = "(.)\\1{${length - 1}}"
-        if (parameter.replaceAll(regexRepetedSequenceOnParameter,"").isEmpty()) {
-            return false
-        } else {
-            return true 
-        }
+        return(!parameter.replaceAll(regexRepetedSequenceOnParameter,"").isEmpty())
     }   
 }
-
-
-
-
-
