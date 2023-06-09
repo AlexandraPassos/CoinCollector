d034f51f5665ada9cef81c1d0ddff7bdc03e9258
Alexandra <alexandra.passos@asaas.com.br>
2023-06-09 16:03:57 -0300

    Arrumando regex de e-mail

--- src/main/groovy/utils/email/EmailUtils.groovy
+++ src/main/groovy/utils/email/EmailUtils.groovy
@@ -3,6 +3,6 @@ package utils.email
 class EmailUtils {
 
     public static Boolean emailIsValid(String email) {
-        return !(email.isEmpty()) && email.matches(/^[a-z0-9\-\.]+@+[a-z0-9]+(\.com)?(\.br)/)
+        return !(email.isEmpty()) && email.matches(/^[a-z0-9\-\.]+@+[a-z0-9]+(\.com)?(\.br)*/)
     }
 }
