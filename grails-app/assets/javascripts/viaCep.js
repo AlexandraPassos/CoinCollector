4d1ce3a000fc7ac6e8f9f83f5781a7816e27ca8f
Alexandra <alexandra.passos@asaas.com.br>
2023-06-19 10:25:10 -0300

    Invertendo lógica e aplicando early return

--- grails-app/assets/javascripts/viaCep.js
+++ grails-app/assets/javascripts/viaCep.js
@@ -1,31 +1,29 @@
 function cepSearch(valor) {
     let cep = valor.replace(/\D/g, '');
-    if (cep !== "") {
-        let validateCep = /^[0-9]{8}$/;
-        if(validateCep.test(cep)) {
-            waitingFormFields();
-            fetchCepData(cep);
-        } else {
-            cleanAddressFields();
-            alert("Formato de CEP inválido.");
-        }
-    } else {
+    if (cep === "") {
         cleanAddressFields();
+        return;
     }
+    let validateCep = /^[0-9]{8}$/;
+    if (!validateCep.test(cep)) {
+        cleanAddressFields();
+        alert("Formato de CEP inválido.");
+        return;
+    }
+    waitingFormFields();
+    fetchCepData(cep);
 }
-
 function viaCepReceiver(json) {
-    if (!("erro" in json)) {
-        document.getElementById('address').value=(json.logradouro);
-        document.getElementById('district').value=(json.bairro);
-        document.getElementById('city').value=(json.localidade);
-        document.getElementById('state').value=(json.uf);
-    } else {
+    if ("erro" in json) {
         cleanAddressFields();
         alert("CEP inválido");
+        return;
     }
+    document.getElementById('address').value=(json.logradouro);
+    document.getElementById('district').value=(json.bairro);
+    document.getElementById('city').value=(json.localidade);
+    document.getElementById('state').value=(json.uf);
 }
-
 function fetchCepData(cep) {
     let script = document.createElement('script');
     script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=viaCepReceiver';
