21f7286e954dafcf0ccf4efdb3266b4464e30443
Alexandra <alexandra.passos@asaas.com.br>
2023-06-22 10:23:39 -0300

    Fazendo correções no try catch do controller

--- grails-app/controllers/com/coinCollector/CustomerController.groovy
+++ grails-app/controllers/com/coinCollector/CustomerController.groovy
@@ -17,18 +17,24 @@ class CustomerController {
 
     def save() {
         try {
-            Long id = params.long("id")
             customerService.save(params)
+
             flash.message = "Cliente registrado com sucesso"
-            redirect(action: 'index', id: id)
+            flash.type = MessageType.SUCCESS
+
+            redirect(action: 'index')
         } catch (ValidationException validationException) {
             flash.message = message(error: validationException.errors.allErrors[0])
             flash.type = MessageType.ERROR
+
             redirect(action: 'create')
         } catch (Exception exception) {
             log.error("CustomerController.save >> Erro ao salvar cliente", exception)
+
             flash.message = "Erro ao salvar cliente"
             flash.type = MessageType.ERROR
+
+            redirect(action: 'create')
         }
     }
 }
