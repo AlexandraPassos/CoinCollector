25e6574524ad77f4e76354a40fef57366dea65da
Alexandra <alexandra.passos@asaas.com.br>
2023-06-28 16:18:24 -0300

    Adicionando método de validação de senha no user

--- grails-app/services/com/coincollector/authentication/UserService.groovy
+++ grails-app/services/com/coincollector/authentication/UserService.groovy
@@ -3,16 +3,31 @@ package com.coincollector.authentication
 import com.coincollector.auth.User
 import com.coincollector.customer.Customer
 import grails.gorm.transactions.Transactional
+import grails.validation.ValidationException
 
 @Transactional
 class UserService {
 
     public void save(Customer customer, String email, String password) {
+        User validatedPassword = validatePassword(password)
+
+        if (validatedPassword.hasErrors()) throw new ValidationException(null, validatedPassword.errors)
+
         User user = new User()
         user.customer = customer
         user.username = email
         user.password = password
 
         user.save(failOnError: true)
+    }
+
+    private User validatePassword(password) {
+        User validatedPassword = new User()
+
+        if (!password) {
+            validatedPassword.errors.reject("", null, "O campo senha é obrigatório")
+        }
+
+        return validatedPassword
     }
 }
